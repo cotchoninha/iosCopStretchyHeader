7a61fad027920e65bb22c732b0c9f7fd749b6462
mauslenter <marcela.auslenter@theappbusiness.com>
2019-04-08 13:31:57 +0100

    Review changes

--- iOScopStretchyHeader/StretchyHeaderCustomLayout.swift
+++ iOScopStretchyHeader/StretchyHeaderCustomLayout.swift
@@ -20,7 +20,7 @@ class StretchyHeaderCustomLayout: UICollectionViewFlowLayout {
 		//you need to access the collection view frame
 		
 		//you need to set your attributes frame (try it out first with some random numbers to see what happens). Cool, now that you know how it behaves think of a way to track down the content offset y of your collection view for when you're dragging it down. ***there're more tips if needed in the end....//1//
-		//considering that you want your image to expand when you drag it down you need to set it's height to the result of your desired default height + your content offset Y - run your code to see the behaviour *** not working? check the tip in the bottom of the page//2//
+		//considering that you want your image to expand when you drag it down you need to set it's height to the result of the attributes frame height + your content offset Y - run your code to see the behaviour *** not working? check the tip in the bottom of the page//2//
 		//ok, looks good, but you don't want this top image to loose from the top of the screen, so make sure you fix that. *** not working? check the tip in the bottom of the page//3//
 		
 		//YEI - almost there. Run your code and interact with your collection view. Notice that there's a bug in your UI. Can you figure it out  on your own?***tip on the bottom if needed//4//
@@ -35,5 +35,5 @@ class StretchyHeaderCustomLayout: UICollectionViewFlowLayout {
 
 //3//Set your frame y to your content OffsetⓎ.
 
-//4//dragging your collection view up will make your offsetY positive and you don't want that. So, make sure you return it before it gets positive.👍🏼
+//4//dragging your collection view up will make your offsetY positive and you don't want that. So, make sure you return the function before it gets positive.👍🏼
 

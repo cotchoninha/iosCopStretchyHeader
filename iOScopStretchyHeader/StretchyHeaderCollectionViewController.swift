bc2056e823f27ede70690dcd5d6a3fee81c2f500
mauslenter <marcela.auslenter@theappbusiness.com>
2019-04-09 13:02:23 +0100

    Added numbers to comments

--- iOScopStretchyHeader/StretchyHeaderCollectionViewController.swift
+++ iOScopStretchyHeader/StretchyHeaderCollectionViewController.swift
@@ -23,7 +23,7 @@ class StretchyHeaderCollectionViewController: UICollectionViewController, UIColl
 	
 	fileprivate func setupCollectionView() {
 		collectionView.backgroundColor = .white
-		//////////use contentInsetAdjustmentBehavior to tell the collection view to ignore the edges underneath the nodge and also the home button
+		//MCA: - use contentInsetAdjustmentBehavior to tell the collection view to ignore the edges underneath the nodge and also the home button
 		collectionView.contentInsetAdjustmentBehavior = .never
 		collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 		collectionView.register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
@@ -37,7 +37,6 @@ class StretchyHeaderCollectionViewController: UICollectionViewController, UIColl
 	
 	override var preferredStatusBarStyle: UIStatusBarStyle {
 		return .lightContent
-		//MARK: - SET COLLECTION VIEW BACKGROUND COLOUR TO SOMETHING DIFF
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -52,13 +51,19 @@ class StretchyHeaderCollectionViewController: UICollectionViewController, UIColl
 	
 	//STEP ②.②
 	override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-		//again, you need to get your scrollView content offset y to play with your effect. If you want print it out to see what this value actually is.
-		//remember from the previous step you had to play with your animator property to change the intensity of your blur? Yep, so now you'll have to use it.
-		//you have to somehow, access your headerView object within this function in order to set its fraction animator property here.***Stuck? //1//
-		//now that you have your header, find its animator and sets its blur to some random number. Run your code and see the result.
-		//Cool, now comes the tricky part. What you actually want is the blur animation to be executed dependent on your offset, right? So as you can see in your debugger screen the content offset is from 0 to negative something, correct? That means that you'll need to get the (1)decimal value of the (2)absolute value of your content offset. ***Stuck? //2//
-		//just to give you a little bit of explanation on what's going on: As you scroll down you get your visual blur effect to gradually blur and once you let go you'll see as the content offset y goes back down to 0 it takes the decimal value of the fraction complete and brings it back to 0 as well.
-		//GREAT! your code is working, now work out your bonus. Make it non blur when you scroll up. Good luck//3//
+		//(1)again, you need to get your scrollView content offset y to play with your effect. If you want print it out to see what this value actually is.
+		
+		//(2)remember from the previous step you had to play with your animator property to change the intensity of your blur? Yep, so now you'll have to use it.
+		
+		//(3)you have to somehow, access your headerView object within this function in order to set its fraction animator property here.***Stuck? //1//
+		
+		//(4)now that you have your header, find its animator and sets its blur to some random number. Run your code and see the result.
+		
+		//(5)Cool, now comes the tricky part. What you actually want is the blur animation to be executed dependent on your offset, right? So as you can see in your debugger screen the content offset is from 0 to negative something, correct? That means that you'll need to get the (1)decimal value of the (2)absolute value of your content offset. ***Stuck? //2//
+		
+		//(6)just to give you a little bit of explanation on what's going on: As you scroll down you get your visual blur effect to gradually blur and once you let go you'll see as the content offset y goes back down to 0 it takes the decimal value of the fraction complete and brings it back to 0 as well.
+		
+		//(7)GREAT! your code is working, now work out your bonus. Make it non blur when you scroll up. Good luck//3//
 	}
 	
 	// MARK: UICollectionViewDataSource
@@ -78,6 +83,10 @@ class StretchyHeaderCollectionViewController: UICollectionViewController, UIColl
 	}
 }
 
+//***TIPS***//
+
 //1//create a property to be an optional of headerView and in your dequeue element function make it return your headerView (don't forget to cast it)
+
 //2// use the fuction abs on your content offset and divide it by 100
+
 //3//you've already implemented something very very similar before in your code
--- iOScopStretchyHeader/StretchyHeaderCustomLayout.swift
+++ iOScopStretchyHeader/StretchyHeaderCustomLayout.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class StretchyHeaderCustomLayout: UICollectionViewFlowLayout {
 	
-	//use the function layoutAttributesForElements to modify the attributes of your header component
+	//MCA: - use the function layoutAttributesForElements to modify the attributes of your header component
 	override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
 		let layoutAttributes = super.layoutAttributesForElements(in: rect)
 		
-		//loop through your layoutAttributes in order to set the necessary attributes
-		//check if the represented element of the attributes you're trying to customize is your elementKindSectionHeader, if yes:
+		//(1)loop through your layoutAttributes in order to set the necessary attributes
 		
-		//you need to access the collection view frame
+		//(2)check if the represented element of the attributes you're trying to customize is your elementKindSectionHeader, if yes:
 		
-		//you need to set your attributes frame (try it out first with some random numbers to see what happens). Cool, now that you know how it behaves think of a way to track down the content offset y of your collection view for when you're dragging it down. ***there're more tips if needed in the end....//1//
-		//considering that you want your image to expand when you drag it down you need to set it's height to the result of the attributes frame height + your content offset Y - run your code to see the behaviour *** not working? check the tip in the bottom of the page//2//
-		//ok, looks good, but you don't want this top image to loose from the top of the screen, so make sure you fix that. *** not working? check the tip in the bottom of the page//3//
+		//(3)you need to access the collection view frame
 		
-		//YEI - almost there. Run your code and interact with your collection view. Notice that there's a bug in your UI. Can you figure it out  on your own?***tip on the bottom if needed//4//
+		//(4)you need to set your attributes frame (try it out first with some random numbers to see what happens). Cool, now that you know how it behaves think of a way to track down the content offset y of your collection view for when you're dragging it down. ***there are more tips if needed in the end....//1//
+		
+		//(5)considering that you want your image to expand when you drag it down you need to set it's height to the result of the attributes frame height + your content offset Y - run your code to see the behaviour *** not working? check the tip in the bottom of the page//2//
+		//(6)ok, looks good, but you don't want this top image to loose from the top of the screen, so make sure you fix that. *** not working? check the tip in the bottom of the page//3//
+		
+		//(7)YEI, almost there. Run your code and interact with your collection view. Notice that there's a bug in your UI. Can you figure it out  on your own?***tip on the bottom if needed//4//
+		
+		//(8)//Now jump to the Header View...
+		
 		return layoutAttributes
 	}
 }
 
-//1//your collection view has a property for its y offset. Get that and print it out to see your offset changing everytime you drag it down.
-//1//Not printing all the time? Don't forget to reset your bounds to get it to calculate every time you interact with it - there's a method that returns a boolean to invalidate your layout. (shouldInvalidateLayout)
+//***TIPS***//
+
+//1.1//your collection view has a property for its y offset. Get that and print it out to see your offset changing everytime you drag it down.
+//1.2//Not printing all the time? Don't forget to reset your bounds to get it to calculate every time you interact with it - there's a method that returns a boolean to invalidate your layout. (shouldInvalidateLayout)
 
 //2//Don't forget your content offset when dragging it down is negative. So make it sum to your height not subtract ✚
 
